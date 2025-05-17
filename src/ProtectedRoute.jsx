1eec4ce28a4c598c6e2e777df6558739c3b43403
mkgdevelopers <muaaznaeemoff@gmail.com>
2025-05-18 01:36:32 +0500

    admin route fix

--- src/ProtectedRoute.jsx
+++ src/ProtectedRoute.jsx
@@ -4,7 +4,7 @@ import { Navigate } from 'react-router-dom';
 const ProtectedRoute = ({ children }) => {
   const isLoggedIn = localStorage.getItem('adminLoggedIn') === 'true';
 
-  return isLoggedIn ? children : <Navigate to="/appointment-booking-page/login" />;
+  return isLoggedIn ? children : <Navigate to="/login" />;
 };
 
 export default ProtectedRoute;
