546b8ffa81baeea3de8180901b762e822975cb5c
mkgdevelopers <muaaznaeemoff@gmail.com>
2025-05-18 01:40:11 +0500

    admin route fix

--- src/admin/components/Sidebar/Sidebar.jsx
+++ src/admin/components/Sidebar/Sidebar.jsx
@@ -6,9 +6,9 @@ const Sidebar = () => {
     <div className="admin-sidebar">
       <h2 className="admin-logo">Admin</h2>
       <ul className="admin-nav">
-        <li><a href="/appointment-booking-page/admin"> Dashboard </a> </li>
-        <li><a href="/appointment-booking-page/admin/all-blogs"> Blogs </a> </li>
-        <li><a href="/appointment-booking-page/admin/appointments"> Appointments </a> </li>
+        <li><a href="/admin"> Dashboard </a> </li>
+        <li><a href="/admin/all-blogs"> Blogs </a> </li>
+        <li><a href="/admin/appointments"> Appointments </a> </li>
       </ul>
     </div>
   );
